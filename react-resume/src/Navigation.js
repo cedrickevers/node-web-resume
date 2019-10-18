c447000567b0b1dd4e9cb0e7b1a102c8c0557770
cedrickevers <keverscedric@gmail.com>
2019-10-18 13:07:55 +0200

    set a sample navbar and sample navigation

---
+++ react-resume/src/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function Navigation() {
+  return (
+<div>
+
+<nav className="flex-container">
+
+<h2 className="nav-logo">Online Resume</h2>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+</nav>
+</div>
+  );
+}
+
+export default Navigation;
